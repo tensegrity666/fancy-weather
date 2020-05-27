2b55ce5d6f58c05197cca8ef3628e92482ea5671
Roman Yakovlev <tensegrity666@eicloud.com>
2020-05-27 23:39:18 +0300

    init: setup environment

---
+++ fancy-weather/webpack.config.js
@@ -0,0 +1,129 @@
+const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+const REGEXP = /\.module.(sc|c)ss$/;
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      {
+        from: 'src/assets/',
+        to: 'assets/',
+      },
+    ]),
+    new HtmlWebpackPlugin({
+      title: 'Caching',
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[contenthash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: REGEXP,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName='[name]__[local]_[hash:base64:5]',
+              esModule: true,
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        exclude: REGEXP,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: true,
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          esModule: true,
+          attributes: true,
+        },
+      },
+    ],
+  },
+};
